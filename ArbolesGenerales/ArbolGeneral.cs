a76a1576a3bee9dd710a8cf0154bdb9d807e85b4
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2020-11-07 14:03:38 -0700

    Insertando nodos

---
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArbolesGenerales
+{
+    public class ArbolGeneral
+    {
+        private readonly Nodo raiz;
+        public Nodo Raiz => raiz;
+
+        public ArbolGeneral(string dato)
+        {
+            this.raiz = new Nodo(dato);
+        }
+
+        public Nodo InsertarNodo(string dato, Nodo nodoPadre)
+        {
+            if (nodoPadre == null) 
+            {
+                throw new Exception($"No se pudo insertar el valor " +
+                    $"{dato}, se debe especificar el nodo padre");
+            }
+
+            if (nodoPadre.Hijo == null)
+            {
+                nodoPadre.Hijo = new Nodo(dato);
+                return nodoPadre.Hijo;
+            }
+            else 
+            {
+                Nodo hijoNodoPadre = nodoPadre.Hijo;
+                while(hijoNodoPadre.Hermano != null) 
+                {
+                    hijoNodoPadre = hijoNodoPadre.Hermano;
+                }
+                hijoNodoPadre.Hermano = new Nodo(dato);
+                return hijoNodoPadre.Hermano;
+            }
+        }
+    }
+}
--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -6,8 +6,14 @@ namespace ArbolesGenerales
     {
         static void Main(string[] args)
         {
+            ArbolGeneral arbol = new ArbolGeneral("A");
+            Nodo nodoB = arbol.InsertarNodo("B", arbol.Raiz);
+            Nodo nodoD = arbol.InsertarNodo("D", nodoB);
+            Nodo nodoE = arbol.InsertarNodo("E", nodoB);
 
-          
+            Console.WriteLine("");
         }
     }
 }
+
+
