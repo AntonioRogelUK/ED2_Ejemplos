4d4ba64dcefcebf7ed232463694cea297f1e3827
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2020-11-14 13:58:58 -0700

    Recorridos Preoden, inorden, posorden

--- ArbolesBinarios/ArbolBinarioBusqueda.cs
+++ ArbolesBinarios/ArbolBinarioBusqueda.cs
@@ -66,5 +66,84 @@ namespace ArbolesBinarios
                 }
             }
         }
+
+        public string Recorrido(Nodo nodo = null, TipoRecorrido tipoRecorrido = TipoRecorrido.Preorden) 
+        {
+            nodo ??= this.raiz;
+            string datos = string.Empty;
+            switch (tipoRecorrido) 
+            {
+                case TipoRecorrido.Preorden:
+                    RecorridoPreorden(nodo, ref datos);
+                    break;
+                case TipoRecorrido.Inorden:
+                    RecorridoInorden(nodo, ref datos);
+                    break;
+                case TipoRecorrido.Posorden:
+                    RecorridoPosorden(nodo, ref datos);
+                    break;
+            }
+            return $"{tipoRecorrido}: {datos}";
+        }
+
+        private void RecorridoPreorden(Nodo nodo, ref string datos)
+        {
+            if (nodo != null)
+            {
+                AgregarDato(nodo, ref datos);
+
+                if (nodo.HijoIzquierdo != null)
+                {
+                    RecorridoPreorden(nodo.HijoIzquierdo, ref datos);
+                }
+
+                if (nodo.HijoDerecho != null)
+                {
+                    RecorridoPreorden(nodo.HijoDerecho, ref datos);
+                }
+            }
+        }
+
+        private void RecorridoInorden(Nodo nodo, ref string datos)
+        {
+            if (nodo != null) 
+            {
+                if (nodo.HijoIzquierdo != null) 
+                {
+                    RecorridoInorden(nodo.HijoIzquierdo, ref datos);
+                }
+
+                AgregarDato(nodo, ref datos);
+
+                if (nodo.HijoDerecho != null) 
+                {
+                    RecorridoInorden(nodo.HijoDerecho, ref datos);
+                }
+            }
+        }
+
+        private void RecorridoPosorden(Nodo nodo, ref string datos)
+        {
+           if (nodo != null) 
+            {
+                if (nodo.HijoIzquierdo != null)
+                {
+                    RecorridoPosorden(nodo.HijoIzquierdo, ref datos);
+                }
+
+                if (nodo.HijoDerecho != null) 
+                {
+                    RecorridoPosorden(nodo.HijoDerecho, ref datos);
+                }
+
+                AgregarDato(nodo, ref datos);
+            }
+        }
+
+        private void AgregarDato(Nodo nodo, ref string datos) 
+        {
+            string coma = (datos == string.Empty) ? string.Empty : ",";
+            datos += $"{coma}{nodo.Dato}";
+        }
     }
 }
--- ArbolesBinarios/Program.cs
+++ ArbolesBinarios/Program.cs
@@ -6,13 +6,32 @@ namespace ArbolesBinarios
     {
         static void Main(string[] args)
         {
-            ArbolBinarioBusqueda arbol = new ArbolBinarioBusqueda(35);
-            arbol.InsertarNodo(20);
+            ArbolBinarioBusqueda arbol = new ArbolBinarioBusqueda(5);
             arbol.InsertarNodo(15);
-            arbol.InsertarNodo(40);
-            arbol.InsertarNodo(25);
+            arbol.InsertarNodo(20);
+            arbol.InsertarNodo(18);
+            arbol.InsertarNodo(80);
+            arbol.InsertarNodo(64);
+            arbol.InsertarNodo(100);
+            arbol.InsertarNodo(19);
+            arbol.InsertarNodo(35);
+            arbol.InsertarNodo(50);
+            arbol.InsertarNodo(90);
+            arbol.InsertarNodo(43);
+            arbol.InsertarNodo(22);
+            arbol.InsertarNodo(1);
+            arbol.InsertarNodo(3);
+            arbol.InsertarNodo(8);
+
+
 
             Console.Write(arbol.ObtenerArbol());
+
+            Console.WriteLine("");
+            Console.WriteLine("--------Recorridos-------");
+            Console.WriteLine(arbol.Recorrido());
+            Console.WriteLine(arbol.Recorrido(tipoRecorrido: ArbolBinarioBusqueda.TipoRecorrido.Inorden));
+            Console.WriteLine(arbol.Recorrido(null, ArbolBinarioBusqueda.TipoRecorrido.Posorden));
         }
     }
 }
